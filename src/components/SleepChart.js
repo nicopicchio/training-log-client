d26147b3545ec7904d9843f8b4c2f3baeca38418
Nico <nicolapicchio@gmail.com>
2022-07-12 09:30:10 +0100

    trying to make the chart work

--- src/components/SleepChart.js
+++ src/components/SleepChart.js
@@ -1,11 +1,15 @@
-import { Chart as ChartJS } from 'chart.js/auto';
+import { useState, useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
+import { Chart as ChartJS, LineElement, PointElement, LinearScale, Title } from 'chart.js';
+
+ChartJS.register(LineElement, PointElement, LinearScale, Title);
 
 function SleepChart() {
-	const labels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-	const datasets = [5, 8, 9, 6, 7, 8, 6, 9];
-	const chartData = { labels, datasets };
-	return <Line data={chartData} />;
+	const [data, setData] = useState({
+		labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+		data: [5, 8, 9, 6, 7, 8, 6, 9]
+	})
+	return <Line data={data} />;
 }
 
 export default SleepChart;
